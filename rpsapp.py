bb3d5e1dd7ce0a0a610adb123b7d49a43cbac069
leahykieran <86846647+leahykieran@users.noreply.github.com>
2025-02-24 12:08:53 -0600

    Add files via upload
    simple rps game add bot

---
+++ rpsapp.py
@@ -0,0 +1,68 @@
+from enum import Enum
+from random import randint
+
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+    def __str__(self):
+        return self.name
+
+
+class RPSGame:
+    def __init__(self):
+        self.player_score = 0
+        self.computer_score = 0
+        self.tie_score = 0
+        self.player_choice = None
+        self.computer_choice = None
+        print("Game is starting...")
+    def player_input(self):
+        while True:
+            try:
+                choice = int(input("Enter your choice: 1. Rock 2. Paper 3. Scissors\n"))
+                if choice in (1, 2, 3):
+                    self.player_choice = RPS(choice)
+                    break
+                else:
+                    print("Invalid choice. Please enter 1, 2, or 3.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
+    def computer_input(self):
+        self.computer_choice = RPS(randint(1, 3))
+
+    def check_winner(self):
+        print(f"You chose: {self.player_choice}, Computer chose: {self.computer_choice}")
+        if self.player_choice == self.computer_choice:
+            self.tie_score += 1
+            print("It's a tie!")
+        elif (self.player_choice == RPS.ROCK and self.computer_choice == RPS.SCISSORS) or \
+             (self.player_choice == RPS.PAPER and self.computer_choice == RPS.ROCK) or \
+             (self.player_choice == RPS.SCISSORS and self.computer_choice == RPS.PAPER):
+            self.player_score += 1
+            print("You win!")
+        else:
+            self.computer_score += 1
+            print("Computer wins!")
+
+    def play(self):
+        while True:
+            self.player_input()
+            self.computer_input()
+            self.check_winner()
+            print(f"Score - Player: {self.player_score}, Computer: {self.computer_score}, Ties: {self.tie_score}")
+            play_again = input("Do you want to play again? (Y/N): ").strip().lower()
+            if play_again != 'y':
+                print("Game Over. Thanks for playing!")
+                break
+
+
+if __name__ == "__main__":
+    game = RPSGame()
+    game.play()
+    input("Press Enter to exit...")
+    
+
